642564df547ee964d16ccc3f3a2f09b754737288
Vitaliy <vlasov.v@sibtrans.ru>
2025-07-04 12:41:52 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// Components
+import Area from '@components/Area.vue';
+import Item from '@components/Item.vue';
+</script>
+
+<template>
+  <div class="main-app">
+    <Area>
+      <Item />
+    </Area>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Area.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="area">
+	area
+	<slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
